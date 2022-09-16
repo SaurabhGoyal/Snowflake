f9bbc0a84497170487001cb20841865f9aae8bbd
Saurabh Goyal <saurabh.goyal@flip.id>
2022-09-16 21:55:08 +0530

    lint fixes

--- snowflake/config.go
+++ snowflake/config.go
@@ -1,47 +1,61 @@
 package snowflake
 
 import (
-	"errors"
 	"fmt"
 	"time"
 )
 
 // time.Date(2010, 12, 12, 23, 59, 59, 0, time.UTC).UnixMilli()
-const DEFAULT_EPOCH = 1292198399000
-const ID_BITS = uint64(63)
-const MAX_ALLOWED_BITS_FOR_TIMESTAMP_AND_NODE_ID = uint64(59)
-const DEFAULT_TIMESTAMP_BITS = uint64(42)
-const DEFAULT_NODE_ID_BITS = uint64(11)
+const defaultEpoch = 1292198399000
+const uidBitLength = uint64(63)
+const minAllowedBitsForTimestamp = uint64(40)
+const maxAllowedBitsForTimestampAndNodeID = uint64(59)
+const defaultTimestampBits = uint64(42)
+const defaultNodeIDBits = uint64(11)
 
 type generatorConfig struct {
 	epoch          uint64
 	timeStampBits  uint64
 	timeStampShift uint64
-	nodeIdBits     uint64
-	nodeIdShift    uint64
+	nodeIDBits     uint64
+	nodeIDShift    uint64
 }
 
-func InitGeneratorConfig(epoch uint64, timestampBits uint64, nodeIdBits uint64) (generatorConfig, error) {
-	current_ts := uint64(time.Now().UnixMilli())
-	if epoch >= current_ts {
-		return generatorConfig{}, errors.New(
-			fmt.Sprintf("epoch must be in past - given epoch [%d] is in future", epoch),
-		)
+/*
+InitGeneratorConfig generates a config to be used by generator to generate IDs.
+- Use latest possible epoch based on what timestamps are irrelevant by your app. This can increase the years that you can target.
+- Use higher or lower timestamp bits based on how long (generally in years) the lifecycle of a generated unique id should be.
+- Use higher or lower nodeID bits based on how many servers are going to be involved in unique id generation.
+- Above two values directly impact the range of unique ids that one server can generate per millisecond while being within the constraint of 64 bit.
+Choose above wisely as higher range per server gives better performance in high throughput systems.
+*/
+func InitGeneratorConfig(epoch uint64, timestampBits uint64, nodeIDBits uint64) (generatorConfig, error) {
+	currentTs := uint64(time.Now().UnixMilli())
+	if epoch >= currentTs {
+		return generatorConfig{}, fmt.Errorf("epoch must be in past - given epoch [%d] is in future", epoch)
 	}
-	if timestampBits+nodeIdBits > MAX_ALLOWED_BITS_FOR_TIMESTAMP_AND_NODE_ID {
-		return generatorConfig{}, errors.New(
-			fmt.Sprintf("timestamp and nodeid can accommodate maximum [%d] bits", MAX_ALLOWED_BITS_FOR_TIMESTAMP_AND_NODE_ID),
-		)
+	if timestampBits < minAllowedBitsForTimestamp {
+		return generatorConfig{}, fmt.Errorf("timestamp length must be atleast [%d] bits to be able to generate unique ids", minAllowedBitsForTimestamp)
+	}
+	if timestampBits+nodeIDBits > maxAllowedBitsForTimestampAndNodeID {
+		return generatorConfig{}, fmt.Errorf("timestamp and nodeid can accommodate maximum [%d] bits", maxAllowedBitsForTimestampAndNodeID)
 	}
 	return generatorConfig{
 		epoch:          epoch,
 		timeStampBits:  timestampBits,
-		timeStampShift: ID_BITS - timestampBits,
-		nodeIdBits:     nodeIdBits,
-		nodeIdShift:    ID_BITS - (timestampBits + nodeIdBits),
+		timeStampShift: uidBitLength - timestampBits,
+		nodeIDBits:     nodeIDBits,
+		nodeIDShift:    uidBitLength - (timestampBits + nodeIDBits),
 	}, nil
 }
 
+/*
+InitDefaultGeneratorConfig generates a default config suited to current applications.
+- Epoch - 2010/12/12
+- Timestamp bits - 42 (~139 years of id uniqueness)
+- NodeID bits - 11 (2048 servers)
+This in turn means 1048 numbers per milliosecond per server can be generated uniquely
+*/
 func InitDefaultGeneratorConfig() (generatorConfig, error) {
-	return InitGeneratorConfig(uint64(DEFAULT_EPOCH), DEFAULT_TIMESTAMP_BITS, DEFAULT_NODE_ID_BITS)
+	return InitGeneratorConfig(uint64(defaultEpoch), defaultTimestampBits, defaultNodeIDBits)
 }
--- uid/uid.go
+++
@@ -1,5 +0,0 @@
-package uid
-
-type UIDGenerator interface {
-	Get() (uint64, error)
-}
